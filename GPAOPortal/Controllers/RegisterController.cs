53eb5353b44ba469a11f82db0173a7a02a2a8652
benzarti <benzimededdine@hotmail.com>
2019-01-09 14:08:21 +0100

    retour login changes

---
+++ GPAOPortal/Controllers/RegisterController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GPAOPortal.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private SGBDEntities sgbd = new SGBDEntities();
+
+        public ActionResult Create()
+        {
+            ViewBag.RoleID = new SelectList(sgbd.Roles, "RoleID", "RoleName");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserID,FullName,Mobile,Address,Username,Password,RoleID")] UserLogin userLogin)
+        {
+            if (ModelState.IsValid)
+            {
+                sgbd.UserLogin.Add(userLogin);
+                sgbd.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoleID = new SelectList(sgbd.Roles, "RoleID", "RoleName", userLogin.RoleID);
+            return View(userLogin);
+        }
+    }
+}
